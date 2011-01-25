016579068d81496cc312ee92ab92d142b34af401
insino <dongkwan1114@kaist.ac.kr>
2011-01-26 00:17:36 +0900

    get mp3 files

--- ESK4-getmp3.rb
+++ ESK4-getmp3.rb
@@ -3,5 +3,12 @@ require 'open-uri'
 t = Mechanize.new
 t2 = t.get("http://www.pbs.org/newshour/newshour_index.html")
 t3 = t2.body.scan(/<a href="(http:\/\/[\.\w\d\/\-\_]+\.mp3)" /).uniq
-file_name = t3.map {|v| v.map {|j| j.scan(/\/([\w\d\-\_]+)\.mp3/)}}
+file_name = []
+t3.each do |v| 
+	v.each do |j| 
+		tmp = Mechanize.new
+		file_name.push($1) if j.scan(/\/([\w\d\-\_]+)\.mp3/)
+		tmp.get(j).save_as($1+".mp3")
+	end
+end
 
