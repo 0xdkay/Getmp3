11e62f89d06ea86bc3933c4877123133f338a535
insino <dongkwan1114@kaist.ac.kr>
2011-01-26 20:25:14 +0900

    remove error

--- ESK4-getmp3.rb
+++ ESK4-getmp3.rb
@@ -7,16 +7,13 @@ t3 = t2.body.scan(/<a href=['"](http:\/\/[\.\w\d\/\-\_]+\.html)['"]/i).uniq
 d = Dir.new(Dir.pwd).to_a
 
 t3.map do |v| 
-	puts v[0]
 	tmp = t.get(v[0])
 	scf = tmp.search('//div[@class="copy"]/p/text() | //div[@class="copy"]/p/strong/text()')
-        mp3f = tmp.search('//div[@class="mp3"]//p/a/@href').to_s
-	if not mp3f.empty?
-		mp3f[0].scan(/\/([\w\d\-\_]+)\.mp3/i)
+        mp3f = tmp.search('//div[@class="mp3"]//p/a').to_s.scan(/http:\/\/[\w\-\.\/]+mp3/)
+	if not mp3f.empty? and mp3f[0] =~ /\/([\w\d\-\_]+)\.mp3/i
 		scriptname = $1+"_script.txt"
 		mp3name = $1+".mp3"
 		dirname = $1.gsub(/\_[\w\d]+/i,'')
-		puts dirname
 		begin
 			d = Dir.new(Dir.pwd+"/#{dirname}").to_a
 		rescue
@@ -26,5 +23,4 @@ t3.map do |v|
 		t.get(mp3f[0]).save_as(File.join(dirname,mp3name)) if not d.index(mp3name)
 		open(File.join(dirname,scriptname),"w") {|f| f.puts(scf)} if not d.index(scriptname)
 	end
-end
 end
