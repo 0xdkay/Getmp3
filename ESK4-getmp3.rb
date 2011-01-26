b3e3cc3c763462f1051ed5ff217a3e39d980165c
insino <dongkwan1114@kaist.ac.kr>
2011-01-26 20:14:13 +0900

    shorten

--- ESK4-getmp3.rb
+++ ESK4-getmp3.rb
@@ -4,13 +4,13 @@ t = Mechanize.new
 t2 = t.get("http://www.pbs.org/newshour/newshour_index.html")
 t3 = t2.body.scan(/<a href=['"](http:\/\/[\.\w\d\/\-\_]+\.html)['"]/i).uniq
 
-d = Dir.new(Dir.pwd).map{|v| v}
+d = Dir.new(Dir.pwd).to_a
 
 t3.map do |v| 
 	puts v[0]
 	tmp = t.get(v[0])
 	scf = tmp.search('//div[@class="copy"]/p/text() | //div[@class="copy"]/p/strong/text()')
-        mp3f = tmp.search('//div[@class="mp3"]//p/a').to_s.scan(/http:\/\/[\w\d\-\_\.\/]+mp3/)
+        mp3f = tmp.search('//div[@class="mp3"]//p/a/@href').to_s
 	if not mp3f.empty?
 		mp3f[0].scan(/\/([\w\d\-\_]+)\.mp3/i)
 		scriptname = $1+"_script.txt"
@@ -18,24 +18,12 @@ t3.map do |v|
 		dirname = $1.gsub(/\_[\w\d]+/i,'')
 		puts dirname
 		begin
-			d = Dir.new(Dir.pwd+"/#{dirname}").map{|v| v}
+			d = Dir.new(Dir.pwd+"/#{dirname}").to_a
 		rescue
 			%x[mkdir #{dirname}]
-			d = Dir.new(Dir.pwd+"/#{dirname}").map{|v| v}
+			d = Dir.new(Dir.pwd+"/#{dirname}").to_a
 		end
 		t.get(mp3f[0]).save_as(File.join(dirname,mp3name)) if not d.index(mp3name)
-		open(File.join(dirname,scriptname),"w") {|f| f.puts(scf)} if not d.index(scriptname)
-	else if scf.to_s.size != 0 
-		n = v[0].scan(/\/([\w\d\-\_]+)\.html/i)
-		scriptname = "2011"+n[0][0].split('_').reverse.join('_').sub('-','')+"_scipt.txt"
-		dirname = "2011"+n[0][0].split('_').reverse[0]
-		puts dirname
-		begin
-			d = Dir.new(Dir.pwd+"/#{dirname}").map{|v| v}
-		rescue
-			%x[mkdir #{dirname}]
-			d = Dir.new(Dir.pwd+"/#{dirname}").map{|v| v}
-		end
 		open(File.join(dirname,scriptname),"w") {|f| f.puts(scf)} if not d.index(scriptname)
 	end
 end
